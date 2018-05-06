06511f21ca94a1bc82b9a0512388780bca6554ee
Josh Sideris <josh.sideris@gmail.com>
2018-05-06 17:53:13 -0400

    Branding

--- index.js
+++ index.js
@@ -77,7 +77,9 @@ $(function(){
 	//User interface:
 	DOT
 		.div().id("formcontainer").class("container").do()
-			.br().br().h("<a href=\"https://twitter.com/joshsideris\" class=\"twitter-follow-button\" data-show-count=\"false\">Follow @joshsideris</a>").br().br().b("Joshua Sideris'")
+			.br().br().h("<a href=\"https://twitter.com/joshsideris\" class=\"twitter-follow-button\" data-show-count=\"false\">Follow @joshsideris</a>")
+			.div().class("g-ytsubscribe").data("channel", "Bizorke").data("layout", "full").data("theme", "dark").data("count", "hidden")
+			.br().br().b("Bizorke's")
 			.h1().do().h("Advanced Kerbal Space Program Mission Calculator").br().i("Rocket Designer <b>&bull;</b> Mission Planner <b>&bull;</b> Parachute / Heat Shield / Aerocapture Calculator <b>&bull;</b> Simulator").class("subtitle").end()
 				.a("Version " + VERSION).href("./changes.html").target("_blank")
 				.br()
@@ -367,7 +369,7 @@ $(function(){
 														.br()
 														.i("*For parachutes, be sure to replace a few of the MK2-Rs with drogue chutes on heavy crafts.")
 														.br()
-														.i("*Report bugs to josh.sideris@gmail.com.")
+														.i("*Report bugs to bizorke@bizorke.com.")
 													.end()
 											.script(function(){
 												DOT.h2("Launch Simulation & Flight Plan")
